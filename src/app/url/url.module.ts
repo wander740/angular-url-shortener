59699c9de1b67055f75d378d19952432f42d4656
Wanderson Ramos <wandersonramos740@gmail.com>
2023-11-20 21:52:51 -0300

    feat: create module url

---
+++ src/app/url/url.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UrlRoutingModule } from './url-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    UrlRoutingModule
+  ]
+})
+export class UrlModule { }
